cdcf597a1b08e37c28994c2386fb36616ce27d52
Josephlee01 <bluewings113@gmail.com>
2022-07-24 15:57:10 +0800

    220724-01

--- src/components/TW539.js
+++ src/components/TW539.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import shortid from "shortid";
 
 const TW539 = () => {
-  return (
-    <div>TW539</div>
-  )
-}
+  let lottoNumbers = [];
+  for (let i = 0; i < 5; i++) {
+    const parsedNumber = parseInt(Math.random()* 45 +1)
+    if (lottoNumbers.indexOf(parsedNumber) === -1) {
+      
+      lottoNumbers.push(parsedNumber);
+    }
+    
+  }
+  lottoNumbers.sort((a,b) => a-b)
 
-export default TW539
+  return (
+    <div>
+      <h1>TW539</h1>
+      <ul>
+        {lottoNumbers.map(num => {
+          return (
+            <li key={shortid.generate()}>{num}</li>
+        )
+      })}  
+</ul>
+      
+    </div>
+  );
+};
+
+export default TW539;
